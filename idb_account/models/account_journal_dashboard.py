da8e5907d0ab3e3db8d92e652d1327fc7411a8b0
LWS <974835150@qq.com>
2025-01-10 18:57:53 +0800

    idb

--- idb_account/models/account_journal_dashboard.py
+++ idb_account/models/account_journal_dashboard.py
@@ -17,6 +17,17 @@ class IdbAccountUpload(models.TransientModel):
     # 应付日期
     due_date = fields.Date(string='Due Date')
 
+    # 借方科目
+    debit_account_id = fields.Many2one('account.account', string='Debit Account')
+    # 贷方科目
+    credit_account_id = fields.Many2one('account.account', string='Credit Account')
+
+    # 汇总会计明细
+    account_move_ids = fields.One2many('idb.account.collect.move.line', 'account_move_id', string='Account Move')
+
+    # 合计金额
+    total_amount = fields.Float(string='Total Amount')
+
     @api.onchange('file')
     def _onchange_file(self):
         file_content = self.file
@@ -25,99 +36,111 @@ class IdbAccountUpload(models.TransientModel):
             file_data = base64.b64decode(file_content)
             workbook = xlrd.open_workbook(file_contents=file_data)
             sheet = workbook.sheet_by_index(0)
-            row_1 = sheet.row(1)
-            partner_name = row_1[7].value
-            partner_id = self.env['res.partner'].search([('name', '=', partner_name)], limit=1)
-            invoice_payment_term = row_1[11].value
-            # 付款条款
-            invoice_payment_term_id = self.env['account.payment.term'].search([('name', '=', invoice_payment_term)],
-                                                                              limit=1)
-            if invoice_payment_term_id:
-                invoice_payment_term_id = invoice_payment_term_id
-            else:
-                invoice_payment_term_id = self.env['account.payment.term'].create({'name': invoice_payment_term})
-            if partner_id:
-                partner_id = partner_id
-            else:
-                partner_id = self.env['res.partner'].create({
-                    'name': partner_name,
-                    'is_company': True,
-                    'customer_rank': 1,
-                })
-                # 出仓单号
-            stock_out_no = row_1[1].value
-            name = partner_name + stock_out_no
-            currency_name = row_1[9].value
-            currency_id = self.env['res.currency'].search([('name', '=', currency_name)], limit=1).id
-            original_amount = 0
-            exchange_rate = float(row_1[10].value)
-            document_date = row_1[2].value
-            due_date = datetime.strptime(document_date, '%Y-%m-%d') + timedelta(days=invoice_payment_term_id.due_days)
-            account_move = {
-                'name': name,
-                'stock_out_no': stock_out_no,
-                'partner_id': partner_id,
-                'currency_id': currency_id,
-                'original_amount': 0,
-                'exchange_rate': exchange_rate,
-                'invoice_payment_term_id': invoice_payment_term_id.id,
-                'document_date': document_date,
-                'due_date': due_date,
-                'invoice_line_ids': [(0, 0, {
-                    'name': name,
-                    'account_id': partner_id.property_account_receivable_id.id,
-                    'currency_id': currency_id,
-                    'exchange_rate': exchange_rate,
-                    'original_amount': 0,
-                    'debit': 0,
-                }),
-                     (0, 0, {
-                         'name': name,
-                         'account_id': self.env['account.account'].search([('code', 'ilike', '6001')], limit=1).id,
-                         'credit': 0,
-                     }),
-                                     ],
-            }
+            total_amount = 0
+            # 按照出仓单号分组
+            account_move = {}
             # 循环遍历sheet数据，直到出现空行
             for row_idx in range(1, sheet.nrows):
                 row = sheet.row(row_idx)
-                # 价税合计数
-                total_value_tax = row[31].value
-                original_amount += total_value_tax
-            account_move['original_amount'] = original_amount
-            account_move['invoice_line_ids'][0][2]['original_amount'] = original_amount
-            amount_currency = original_amount * exchange_rate
-            account_move['invoice_line_ids'][0][2]['debit'] = amount_currency
-            account_move['invoice_line_ids'][1][2]['credit'] = amount_currency
-            self.update(account_move)
-
-    # 客户
-    partner_id = fields.Many2one('res.partner', string='Customer')
-    # 单号
-    stock_out_no = fields.Char(string='Stock Out No')
-    # 付款条款
-    invoice_payment_term_id = fields.Many2one('account.payment.term', string='Payment Term')
-    # 币种
-    currency_id = fields.Many2one('res.currency', string='Currency')
-    # 汇率
-    exchange_rate = fields.Float(string='Exchange Rate')
-
-    @api.onchange('exchange_rate', 'original_amount')
-    def _onchange_exchange_rate(self):
-        if self.exchange_rate and self.original_amount:
-            for line in self.invoice_line_ids:
-                if line.currency_id:
-                    line.debit = self.original_amount * self.exchange_rate
-                    line.exchange_rate = self.exchange_rate
+                # 出仓单号
+                stock_out_no = row[1].value
+                # 出仓日期
+                document_date = row[2].value
+                # 订货日期
+                order_date = row[3].value
+                # 交货日期
+                delivery_date = row[4].value
+                # 客户
+                partner_name = row[7].value
+                partner_id = self.env['res.partner'].search([('name', '=', partner_name)], limit=1)
+                if partner_id:
+                    partner_id = partner_id
                 else:
-                    line.credit = self.original_amount * self.exchange_rate
-    # 原始金额
-    original_amount = fields.Float(string='Original Amount')
-    # 单据日期
-    document_date = fields.Date(string='Document Date')
-
-    # 会计凭证
-    invoice_line_ids = fields.One2many('idb.account.upload.move.line', 'account_move_id', string='Invoice Line')
+                    partner_id = self.env['res.partner'].create({
+                        'name': partner_name,
+                        'is_company': True,
+                        'customer_rank': 1,
+                    })
+                # 出仓员
+                user_name = row[8].value
+                # 币种
+                currency_name = row[9].value
+                currency_id = self.env['res.currency'].search([('name', '=', currency_name)], limit=1)
+                if not currency_id:
+                    raise UserError(_("Coins %s do not exist, please contact the administrator to maintain the data" % currency_name))
+                # 汇率
+                exchange_rate = float(row[10].value)
+                # 结算方式
+                invoice_payment_term = row[11].value
+                # 付款条款
+                invoice_payment_term_id = self.env['account.payment.term'].search([('name', '=', invoice_payment_term)],
+                                                                                  limit=1)
+                if invoice_payment_term_id:
+                    invoice_payment_term_id = invoice_payment_term_id
+                else:
+                    invoice_payment_term_id = self.env['account.payment.term'].create({'name': invoice_payment_term})
+                # 订单号
+                order_no = row[17].value
+                # 款号
+                style_no = row[18].value
+                # 物料名称
+                material_name = row[20].value
+                # 物料分类
+                material_category = row[24].value
+                # 数量
+                quantity = float(row[27].value)
+                # 单位
+                unit = row[28].value
+                # 单价
+                price_unit = float(row[29].value)
+                # 价税合计
+                line_total_value_tax = float(row[31].value)
+                name = partner_name + stock_out_no
+                line = (0, 0, {
+                    'name': name,
+                    'stock_out_no': stock_out_no,
+                    'order_date': order_date,
+                    'delivery_date': delivery_date,
+                    'document_date': document_date,
+                    'partner_id': partner_id.id,
+                    'user_name': user_name,
+                    'currency_id': currency_id,
+                    'exchange_rate': exchange_rate,
+                    'invoice_payment_term_id': invoice_payment_term_id.id,
+                    'order_no': order_no,
+                    'style_no': style_no,
+                    'material_name': material_name,
+                    'material_category': material_category,
+                    'quantity': quantity,
+                    'unit': unit,
+                    'price_unit': price_unit,
+                    'total_value_tax': line_total_value_tax,
+                })
+                if stock_out_no not in account_move:
+                    account_move[stock_out_no] = {
+                        'name': name,
+                        'stock_out_no': stock_out_no,
+                        'document_date': document_date,
+                        'partner_id': partner_id.id,
+                        'user_name': user_name,
+                        'currency_id': currency_id,
+                        'exchange_rate': exchange_rate,
+                        'invoice_payment_term_id': invoice_payment_term_id.id,
+                        'total_value_tax': line_total_value_tax,
+                        'invoice_line_ids': [line]
+                    }
+                else:
+                    account_move[stock_out_no]['invoice_line_ids'].append(line)
+                    account_move[stock_out_no]['total_value_tax'] += line_total_value_tax
+                total_amount += line_total_value_tax * exchange_rate
+            account_data = {
+                'debit_account_id': self.env['account.account'].search([('code', 'ilike', '1122')], limit=1).id,
+                'credit_account_id': self.env['account.account'].search([('code', 'ilike', '6001')], limit=1).id,
+                'total_amount': total_amount,
+                'account_move_ids': [(0, 0, account_line) for account_line in account_move.values()]
+            }
+            self.update(account_data)
+            return
 
     def action_generate_voucher_purchase(self):
         # 解析文件数据
@@ -153,7 +176,7 @@ class IdbAccountUpload(models.TransientModel):
             }))
         # 创建凭证
         account_move = self.env['account.move'].create(account_move)
-        # 跳转会计凭证试图
+        # 跳转会计凭证视图
         return {
             'type': 'ir.actions.act_window',
             'name': _('Account Move'),
@@ -163,67 +186,143 @@ class IdbAccountUpload(models.TransientModel):
         }
 
     def action_generate_voucher_sale(self):
-        exchange_rate = self.exchange_rate
-        original_amount = self.original_amount
-        currency_id = self.currency_id.id
-        stock_out_no = self.stock_out_no
-        name = self.name
-        account_move = {
-            'move_type': 'out_invoice',
-            'invoice_date': self.document_date,
-            'stock_picking_name': stock_out_no,
-            'partner_id': self.partner_id.id,
-            'currency_id': currency_id,
-            'original_amount': original_amount,
-            'exchange_rate': exchange_rate,
-            'invoice_payment_term_id': self.invoice_payment_term_id.id,
-            'invoice_line_ids': [(0, 0, {
-                'name': name,
+        account_move_data = []
+        for line in self.account_move_ids:
+            exchange_rate = line.exchange_rate
+            original_amount = line.total_value_tax
+            currency_id = line.currency_id.id
+            stock_out_no = line.stock_out_no
+            name = line.name
+            account_move = {
                 'abstract': name,
+                'move_type': 'out_invoice',
+                'invoice_date': line.document_date,
                 'stock_picking_name': stock_out_no,
+                'partner_id': line.partner_id.id,
                 'currency_id': currency_id,
-                'quantity': 1,
-                'price_unit': original_amount * exchange_rate
-            })],
-        }
-        # 创建凭证
-        account_move = self.env['account.move'].create(account_move)
+                'original_amount': original_amount,
+                'exchange_rate': exchange_rate,
+                'invoice_payment_term_id': line.invoice_payment_term_id.id,
+                'invoice_line_ids': [(0, 0, {
+                    'name': name,
+                    'stock_picking_name': stock_out_no,
+                    'currency_id': currency_id,
+                    'quantity': 1,
+                    'price_unit': line.total_amount,
+                })],
+                'gt_stock_move_ids': [(0, 0, {
+                    'name': name,
+                    'stock_out_no': stock_out_no,
+                    'order_date': stock_move_id.order_date,
+                    'delivery_date': stock_move_id.delivery_date,
+                    'document_date': stock_move_id.document_date,
+                    'partner_id': stock_move_id.partner_id.id,
+                    'user_name': stock_move_id.user_name,
+                    'currency_id': currency_id,
+                    'exchange_rate': exchange_rate,
+                    'invoice_payment_term_id': stock_move_id.invoice_payment_term_id.id,
+                    'order_no': stock_move_id.order_no,
+                    'style_no': stock_move_id.style_no,
+                    'material_name': stock_move_id.material_name,
+                    'material_category': stock_move_id.material_category,
+                    'quantity': stock_move_id.quantity,
+                    'unit': stock_move_id.unit,
+                    'price_unit': stock_move_id.price_unit,
+                    'total_value_tax': stock_move_id.total_value_tax,
+                }) for stock_move_id in line.invoice_line_ids],
+            }
+            account_move_data.append(account_move)
+        account_move_ids = self.env['account.move'].create(account_move_data)
         # 跳转会计凭证试图
         return {
             'type': 'ir.actions.act_window',
             'name': _('Account Move'),
             'res_model': 'account.move',
-            'view_mode': 'form',
-            'res_id': account_move.id,
+            'view_mode': 'tree,form',
+            'domain': [('id', 'in', account_move_ids.ids)],
         }
 
     # 生成凭证
     def action_generate_voucher(self):
-        account_type = self.env.context.get('type')
+        account_type = self.env.context.get('account_type')
         if account_type == 'in_invoice':
             return self.action_generate_voucher_purchase()
         elif account_type == 'out_invoice':
             return self.action_generate_voucher_sale()
 
 
+class IdbAccountCollectMoveLine(models.TransientModel):
+    _name = "idb.account.collect.move.line"
+    _description = "IDB Account Upload Move Line"
+
+    name = fields.Char(string='Abstract')
+    account_move_id = fields.Many2one('idb.account.upload', string='Account Move')
+    # 出仓单号
+    stock_out_no = fields.Char(string='The warehouse receipt number')
+    # 出仓日期
+    document_date = fields.Date(string='Exit date')
+    # 客户
+    partner_id = fields.Many2one('res.partner', string='Customer')
+    # 出仓员
+    user_name = fields.Char(string='Warehouse staff')
+    # 币种
+    currency_id = fields.Many2one('res.currency', string='Currency')
+    # 汇率
+    exchange_rate = fields.Float(string='Exchange Rate')
+
+    @api.onchange('exchange_rate')
+    def _onchange_exchange_rate(self):
+        for record in self:
+            record.invoice_line_ids.exchange_rate = record.exchange_rate
+
+    # 付款条款
+    invoice_payment_term_id = fields.Many2one('account.payment.term', string='Payment Term')
+
+    @api.onchange('invoice_payment_term_id')
+    def _onchange_invoice_payment_term_id(self):
+        for record in self:
+            record.invoice_line_ids.invoice_payment_term_id = record.invoice_payment_term_id.id
+
+    # 摘要
+    abstract = fields.Char(string='Abstract')
+    # 价税合计
+    total_value_tax = fields.Float(string='Total value-added tax')
+    # 总金额
+    total_amount = fields.Float(string='Total Amount', compute='_compute_total_amount')
+
+    @api.depends('total_value_tax', 'exchange_rate')
+    def _compute_total_amount(self):
+        for record in self:
+            record.total_amount = record.total_value_tax * record.exchange_rate
+
+    invoice_line_ids = fields.One2many('idb.account.upload.move.line', 'account_move_id', string='Billing summary')
+
+
 class IdbAccountUploadMoveLine(models.TransientModel):
     _name = "idb.account.upload.move.line"
     _description = "IDB Account Upload Move Line"
 
-    account_move_id = fields.Many2one('idb.account.upload', string='Account Move')
-    name = fields.Char(string='abstract')
-    # 会计科目
-    account_id = fields.Many2one('account.account', string='Account')
-    # 借方
-    debit = fields.Float(string='Debit')
-    # 贷方
-    credit = fields.Float(string='Credit')
-    # 原币金额
-    original_amount = fields.Float(string='Amount Currency')
-    # 汇率
-    exchange_rate = fields.Float(string='Exchange Rate')
-    # 币别
+    name = fields.Char(string='Abstract')
+    account_move_id = fields.Many2one('idb.account.collect.move.line', string='Account Move')
+    stock_out_no = fields.Char(string='The warehouse receipt number')
+    document_date = fields.Date(string='Exit date')
+    order_date = fields.Date(string='Order date')
+    delivery_date = fields.Date(string='Delivery date')
+    partner_id = fields.Many2one('res.partner', string='Customer')
+    user_name = fields.Char(string='Warehouse staff')
     currency_id = fields.Many2one('res.currency', string='Currency')
+    exchange_rate = fields.Float(string='Exchange Rate')
+    order_no = fields.Char(string='Order number')
+    style_no = fields.Char(string='Style number')
+    material_name = fields.Char(string='Material name')
+    material_category = fields.Char(string='Material category')
+    quantity = fields.Float(string='Quantity')
+    unit = fields.Char(string='Unit')
+    price_unit = fields.Float(string='Unit price')
+    total_value_tax = fields.Float(string='Total value-added tax')
+    # 付款条款
+    invoice_payment_term_id = fields.Many2one('account.payment.term', string='Payment Term')
+
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"
--- idb_account/models/account_move.py
+++ idb_account/models/account_move.py
@@ -21,6 +21,10 @@ class AccountMove(models.Model):
     exchange_rate = fields.Float(string='Exchange Rate')
     # 原始金额
     original_amount = fields.Float(string='Original Amount')
+    abstract = fields.Char(string='Abstract')
+
+    # 导入数据明细
+    gt_stock_move_ids = fields.One2many('idb.account.move.line', 'move_id', string='Import Data Details')
 
 
 class AccountMoveLine(models.Model):
@@ -28,4 +32,30 @@ class AccountMoveLine(models.Model):
 
     # 库存单号
     stock_picking_name = fields.Char(string='Warehouse Bill Number')
-    abstract = fields.Char(string='Abstract')
+    abstract = fields.Char(string='Abstract', related='move_id.abstract')
+
+
+class IdbAccountMoveLine(models.Model):
+    _name = "idb.account.move.line"
+    _description = "IDB Account Upload Move Line"
+
+    move_id = fields.Many2one('account.move', string='Account Move')
+    name = fields.Char(string='Abstract')
+    stock_out_no = fields.Char(string='The warehouse receipt number')
+    document_date = fields.Date(string='Exit date')
+    order_date = fields.Date(string='Order date')
+    delivery_date = fields.Date(string='Delivery date')
+    partner_id = fields.Many2one('res.partner', string='Customer')
+    user_name = fields.Char(string='Warehouse staff')
+    currency_id = fields.Many2one('res.currency', string='Currency')
+    exchange_rate = fields.Float(string='Exchange Rate')
+    order_no = fields.Char(string='Order number')
+    style_no = fields.Char(string='Style number')
+    material_name = fields.Char(string='Material name')
+    material_category = fields.Char(string='Material category')
+    quantity = fields.Float(string='Quantity')
+    unit = fields.Char(string='Unit')
+    price_unit = fields.Float(string='Unit price')
+    total_value_tax = fields.Float(string='Total value-added tax')
+    # 付款条款
+    invoice_payment_term_id = fields.Many2one('account.payment.term', string='Payment Term')
